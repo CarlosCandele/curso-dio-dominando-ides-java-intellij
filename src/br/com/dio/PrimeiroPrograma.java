b4dead164a2fedc59ede723247f4ae044a16c4cd
CarlosCandele <carloscandele@hotmail.com>
2023-02-28 18:25:51 -0300

    Testando a ide intellij.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,7 +1,47 @@
 package br.com.dio;
 
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Hello Intellij");
+        Gato gato = new Gato("Pituco","Branco",2);
+
+
+
+        System.out.println( " Nome: " + gato.getNome() + '\'' + "Cor: "+gato.getCor() + '\''+ " Idade: "+ gato.getIdade());
+
+        Livro livro = new Livro("Codigo Limpo",278);
+        System.out.println(livro);
     }
+}
+
+class Livro{
+    private String titulo;
+    private int paginas;
+
+public Livro(String titulo, int paginas) {
+    this.titulo = titulo;
+    this.paginas = paginas;
+    }
+
+public String getTitulo() {
+    return titulo;
+    }
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+    }
+
+public int getPaginas() {
+    return paginas;
+    }
+public void setPaginas(int paginas) {
+    this.paginas = paginas;
+    }
+
+    public String toString() {
+        return "Livro{" +
+                "Titulo: " + titulo + '\'' +
+                ", Paginas: " + paginas +
+                "}";
+    }
+
 }
